16f73cd57358705e8455f6c5b47c7f641a8031f6
TaranPPatel <taranpatel1006@gmail.com>
2024-11-04 13:38:47 -0600

    changes

--- main.py
+++ main.py
@@ -14,6 +14,37 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
+
+# labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+username_label = Label(text="Email/Username:")
+username_label.grid(row=2, column=0)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+# Inputs
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+username_entry = Entry(width=35)
+username_entry.grid(row=2, column=1, columnspan=2)
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1, columnspan=1)
+
+# Buttons
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
 
 window.mainloop()
