e798a1e9463fe5a0c3f740a3197a0962362dc41e
TaranPPatel <taranpatel1006@gmail.com>
2024-11-04 10:08:04 -0600

    final

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ window.config(padx=20, pady=20)
 # Canvas
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 112, image=logo_img)
+canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=1)
 
 window.mainloop()
